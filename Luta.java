c56119f784bbeac09243dd69b86142718b607fac
maria das claras <61098073+ClaraGomes02@users.noreply.github.com>
2020-11-24 16:41:17 -0300

    Add files via upload

---
+++ Luta.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author clara
+ */
+public class Luta {
+    
+//    Atributos.
+    private String Desafiado;
+    private String Desafiante;
+    private int rounds;
+    private boolean aprovada;
+    
+//    Métodos.
+    public void marcarLuta(){
+        
+        
+        
+    }
+    
+    public void lutar(){
+        
+        
+        
+    }
+
+    public String getDesafiado() {
+        return this.Desafiado;
+    }
+
+    public void setDesafiado(String Do) {
+        this.Desafiado = Do;
+    }
+
+    public String getDesafiante() {
+        return this.Desafiante;
+    }
+
+    public void setDesafiante(String De) {
+        this.Desafiante = De;
+    }
+
+    public int getRounds() {
+        return this.rounds;
+    }
+
+    public void setRounds(int R) {
+        this.rounds = R;
+    }
+
+    public boolean getAprovada() {
+        return this.aprovada;
+    }
+
+    public void setAprovada(boolean AP) {
+        this.aprovada = AP;
+    }
+    
+    
+}
